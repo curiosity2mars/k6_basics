41d4e87876ad80ae522369d30c9636eae04b39e1
Vital Koblik <laptop@Vitals-MacBook-Pro.local>
2023-02-08 21:22:56 +0100

    Initial setup and test

---
+++ script.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '10s',
+  };
+
+export default function () {
+    let response = http.get('https://google.com');
+    check(response, {
+        "Verify Google page is loaded": (r) => r.body.includes("Google"),
+    });
+    sleep(1);
+}
